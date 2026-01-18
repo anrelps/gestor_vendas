9f270ac5620443be6075ee01386c450afee01509
Anrel Penning Siefert <132225545+anrelps@users.noreply.github.com>
2026-01-18 17:16:06 -0300

    Add Clients page and update routing
    Introduced a new Clients page that renders the ClientList component. Updated App.jsx to use the new Clients page for the '/clientes' route instead of rendering ClientList directly. Also improved code style and button labels in the Login page for better UX.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,86 +1,86 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import logo from "./assets/logo.svg";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+import logo from './assets/logo.svg';
 
 // Redux
-import { checkAuth } from "./redux/slices/userSlice";
+import { checkAuth } from './redux/slices/userSlice';
 
 // Pages
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import Clients from './pages/Clients';
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 // Components
-import ClientList from "./components/ClientList";
-import Sidebar from "./components/layout/Sidebar";
-import ProtectedRoute from "./components/ProtectedRoute";
+import Sidebar from './components/layout/Sidebar';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
-  const dispatch = useDispatch();
-  const { user, token } = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    const { user, token } = useSelector((state) => state.user);
 
-  // Checagem se o usuario ja esta autenticado
-  useEffect(() => {
-    if (token) {
-      dispatch(checkAuth());
-    }
-  }, [dispatch, token]);
+    // Checagem se o usuario ja esta autenticado
+    useEffect(() => {
+        if (token) {
+            dispatch(checkAuth());
+        }
+    }, [dispatch, token]);
 
-  const navItems = [
-    {
-      label: "Dashboard",
-      to: "/dashboard",
-      icon: "fa-solid fa-arrows-down-to-people",
-    },
-    {
-      label: "Clientes",
-      to: "/clientes",
-      icon: "fa-regular fa-address-card",
-    },
-    {
-      label: "Notificações",
-      to: "/notificacoes",
-      icon: "fa-solid fa-bell-concierge",
-    },
-    { label: "Livros", to: "/livros", icon: "fa-solid fa-book-bookmark" },
-  ];
+    const navItems = [
+        {
+            label: 'Dashboard',
+            to: '/dashboard',
+            icon: 'fa-solid fa-arrows-down-to-people',
+        },
+        {
+            label: 'Clientes',
+            to: '/clientes',
+            icon: 'fa-regular fa-address-card',
+        },
+        {
+            label: 'Notificações',
+            to: '/notificacoes',
+            icon: 'fa-solid fa-bell-concierge',
+        },
+        { label: 'Livros', to: '/livros', icon: 'fa-solid fa-book-bookmark' },
+    ];
 
-  const authUser = {
-    name: user?.nome || "nome",
-    role: user?.empresa?.nome || "empresa",
-    editTo: "/perfil/editar",
-  };
+    const authUser = {
+        name: user?.nome || 'nome',
+        role: user?.empresa?.nome || 'empresa',
+        editTo: '/perfil/editar',
+    };
 
-  return (
-    <BrowserRouter>
-      <div className="flex min-h-screen">
-        <Sidebar
-          logoSrc={logo}
-          authUser={authUser}
-          navItems={navItems}
-          onLogout={() => {
-            // limpar token/session aqui
-          }}
-        />
-        <main className="flex-1 bg-gray-50 p-6">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route
-              element={
-                <ProtectedRoute>
-                  <Outlet />
-                </ProtectedRoute>
-              }
-            >
-              <Route path="/clientes" element={<ClientList />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-            </Route>
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className='flex min-h-screen'>
+                <Sidebar
+                    logoSrc={logo}
+                    authUser={authUser}
+                    navItems={navItems}
+                    onLogout={() => {
+                        // limpar token/session aqui
+                    }}
+                />
+                <main className='flex-1 bg-gray-50 p-6'>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route
+                            element={
+                                <ProtectedRoute>
+                                    <Outlet />
+                                </ProtectedRoute>
+                            }
+                        >
+                            <Route path='/clientes' element={<Clients />} />
+                            <Route path='/dashboard' element={<Dashboard />} />
+                        </Route>
+                    </Routes>
+                </main>
+            </div>
+        </BrowserRouter>
+    );
 };
 export default App;
---
+++ frontend/src/pages/Clients.jsx
@@ -0,0 +1,11 @@
+import ClientList from '../components/ClientList';
+
+const Clients = () => {
+    return (
+        <div>
+            <ClientList />
+        </div>
+    );
+};
+
+export default Clients;
