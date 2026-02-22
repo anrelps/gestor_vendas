893f387229847302c42688328df43180909d6316
Anrel Penning Siefert <132225545+anrelps@users.noreply.github.com>
2026-02-22 19:16:35 -0300

    Adicionando suporte a CORS para localhost e ajustando baseURL da API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: "http://127.0.0.1:9000/api/v1/",
-  //baseURL: "http://localhost/api/v1/",
-  baseURL: "https://uselumenz.com/api/v1/",
+  baseURL: 'http://127.0.0.1:8000/api/v1/',
+  //baseURL: "https://uselumenz.com/api/v1/",
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
