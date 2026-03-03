8d809fc6e6729672cc3cb6f87fe2c7b1e5a7e7b5
Isaque Oliveira <isaque16.oliveira@gmail.com>
2026-03-03 14:35:56 -0300

    Adicionando opcao de escolher vendas e cliente especifico para atualizar valor pago

--- backend/app/Services/VendaService.php
+++ backend/app/Services/VendaService.php
@@ -109,10 +109,16 @@ class VendaService {
         return $venda->delete();
     }
 
-    public function applyPaymentToMultipleSales(float $value, string $paymentType) {
+    public function applyPaymentToMultipleSales(float $value, string $paymentType, array $data) {
         $vendas = $this->venda
             ->where('empresa_id', auth()->user()->empresa_id)
-            ->whereColumn('valor_pago', '<>', 'valor_total');
+            ->whereColumn('valor_pago', '<>', 'valor_total')
+            ->when(isset($data['cliente']), function($q) use ($data) {
+                $q->where('cliente_id', $data['cliente']);
+            })
+            ->when(isset($data['vendas_ids']), function($q) use ($data) {
+                $q->whereIn('id', $data['vendas_ids']);
+            });
 
         switch($paymentType) {
             case 'split_equally':
